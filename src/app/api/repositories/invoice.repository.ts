7c5f3791b49437a655779e280f8b987a8aca3059
juanzb <juanzarache2000@gmail.com>
2026-06-07 10:34:16 -0500

    improvements invoice repository

--- src/app/api/repositories/invoice.repository.ts
+++ src/app/api/repositories/invoice.repository.ts
@@ -9,4 +9,16 @@ export class InvoiceRepository extends BaseApiService {
   getInvoices(endpoint = 'invoices'): Promise<IInvoiceGet[]> {
     return this.get<IInvoiceGet[]>(endpoint);
   }
+
+  deleteInvoices(endpoint = 'invoices'): Promise<IInvoiceGet[]> {
+    return this.delete<IInvoiceGet[]>(endpoint);
+  }
+
+  createInvoice(invoice: IInvoice, endpoint = 'invoices'): Promise<IInvoice> {
+    return this.post<IInvoice>(endpoint, invoice);
+  }
+
+  updateInvoice(invoice: IInvoice, endpoint = 'invoices'): Promise<IInvoice> {
+    return this.put<IInvoice>(endpoint, invoice);
+  }
 }
