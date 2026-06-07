50bf1a4e2a679cb85d67a06f46ce4c9122b1239a
juanzb <juanzarache2000@gmail.com>
2026-06-07 00:10:10 -0500

    define routes the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'app',
+    children: [
+      {
+        path: 'invoices',
+        loadComponent: () =>
+          import('@view/Invoices/Invoice.component').then((c) => c.InvoiceComponent),
+      },
+      {
+        path: 'recalculate',
+        loadComponent: () =>
+          import('@view/Recalculate/recalculate.component').then((c) => c.RecalculateComponent),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'app/home',
+    pathMatch: 'full',
+  },
+];
