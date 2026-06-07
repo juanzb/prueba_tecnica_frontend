e552203a0440dc4eeca0dff06e47d5d30b101196
juanzb <juanzarache2000@gmail.com>
2026-06-07 10:40:36 -0500

    invoice view refactoring

--- src/app/view/Invoices/Invoice.component.ts
+++ src/app/view/Invoices/Invoice.component.ts
@@ -1,23 +1,38 @@
-import { Component, effect, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { IProduct } from '../../interfaces/IProducts';
 import { IInvoice, IInvoiceGet } from '../../interfaces/IInvoices';
 import { ProductsRepository } from '../../api/repositories/product.repository';
 import { InvoiceRepository } from '../../api/repositories/invoice.repository';
-import { CardProductComponent } from '@components/card-product/cardProduct';
-import { ButtonToggelComponent } from '@components/ui/buttton/button-toggle.component';
 import { CardInvoiceComponent } from '@components/card-invoices/card-invoice.component';
+import { CardInvoiceDetailComponent } from '@components/card-invoice-detail/card-invoice-detail.component';
+import { CurrencyPipe } from '@angular/common';
+import { ModalConfirmComponent } from '@components/ui/modal-confirm/modal-confirm.component';
+import { FromInvoiceComponent } from '@components/form-invoice/formInvoice';
+import { FormRecalculateComponent } from '@components/form-recalculate/form-recalculate.component';
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
   standalone: true,
-  imports: [CardInvoiceComponent],
+  imports: [
+    CardInvoiceComponent,
+    CardInvoiceDetailComponent,
+    CurrencyPipe,
+    ModalConfirmComponent,
+    FromInvoiceComponent,
+    FormRecalculateComponent,
+  ],
 })
 export class InvoiceComponent implements OnInit {
   protected dataProducts = signal<IProduct[]>([]);
-  protected dataInvoices = signal<IInvoiceGet[]>([]);
+  protected dataInvoices = signal<IInvoiceGet[]>([] as IInvoiceGet[]);
   isLoading = signal<boolean>(false);
   isError = signal<boolean>(false);
+  idInvoiceToShow = signal<number>(0);
+  openModalToDeleteInvoice = signal<boolean>(false);
+  idInvoiceToDelete = signal<number>(0);
+  isRecalculate = signal<boolean>(false);
+
   productRepository = inject(ProductsRepository);
   invoiceRepository = inject(InvoiceRepository);
 
@@ -27,23 +42,69 @@ export class InvoiceComponent implements OnInit {
   }
 
   findDataProducts() {
-    this.productRepository.getProducts().then((data) => {
-      console.log(data);
-      this.dataProducts.set(data);
-    });
+    this.productRepository
+      .getProducts()
+      .then((data) => {
+        this.dataProducts.set(data);
+      })
+      .catch((error) => {
+        this.isError.set(true);
+      });
   }
 
   findDataInvoices() {
-    this.invoiceRepository.getInvoices().then((data) => {
-      console.log(data);
-      this.dataInvoices.set(data);
+    this.invoiceRepository
+      .getInvoices()
+      .then((data) => {
+        this.dataInvoices.set(data);
+      })
+      .catch((error) => {
+        this.isError.set(true);
+      });
+  }
+
+  createInvoice(invoice: IInvoice) {
+    this.invoiceRepository
+      .createInvoice(invoice)
+      .then(() => {
+        this.findDataInvoices();
+      })
+      .catch((error) => {
+        this.isError.set(true);
+      });
+  }
+
+  updateInvoice(invoice: IInvoice) {
+    this.invoiceRepository
+      .updateInvoice(invoice)
+      .then(() => {
+        this.findDataInvoices();
+      })
+      .catch((error) => {
+        this.isError.set(true);
+      });
+  }
+
+  checkInvoiceActive(event: number) {
+    this.idInvoiceToShow.update((prev) => {
+      if (prev === event) return -1;
+      return event;
     });
   }
 
-  createInvoice() {
-    this.invoiceRepository.getInvoices().then((data) => {
-      console.log(data);
-      this.dataInvoices.set(data);
+  selectInvoiceToDelete(event: number) {
+    this.idInvoiceToDelete.set(event);
+    this.openModalToDeleteInvoice.set(true);
+  }
+
+  closeModalConfirmDeleteInvoice() {
+    this.openModalToDeleteInvoice.set(false);
+  }
+
+  deleteInvoice() {
+    this.invoiceRepository.deleteInvoices(`invoices/${this.idInvoiceToDelete()}`).then(() => {
+      this.findDataInvoices();
     });
+    this.openModalToDeleteInvoice.set(false);
   }
 }
--- src/app/view/Recalculate/recalculate.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-recalculate',
-  templateUrl: './recalculate.component.html',
-  standalone: true,
-})
-export class RecalculateComponent {}
